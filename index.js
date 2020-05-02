f8daaa1d80a1b887269404f671ba156c008b504c
Wesley <wesaboo@gmail.com>
2020-05-02 12:36:23 -0700

    workign on quiz

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
 var startButton = document.querySelector("#startButton");
 var gameOver = document.querySelectorAll("#gameOver");
-var question = document.querySelector("#question");
+var questionBox = document.querySelector("#question");
 var answer0 = document.querySelector("#answer0");
 var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
 var realAnswer = document.querySelector("#realAnswer");
-var choice= document.querySelectorAll(".choice");
+var choice = document.querySelectorAll(".choice");
 var timer = document.querySelector("#timer");
 var timeContainer = document.querySelector(".timer");
+var score = document.querySelector("#score");
 var scoreContainer = document.querySelector(".score");
 
-var score = 0;
+score.textContent = 0;
+console.log(score.textContent);
 
-function startTimer(){
-timer.textContent = 10;
-setInterval(function (){
-    timer.textContent--;
-},1000);
+function startTimer() {
+    timer.textContent = 10;
+    setInterval(function () {
+        timer.textContent--;
+    }, 1000); // how do I get the timer to stop at 0?
 };
 
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", function () {
     this.setAttribute("class", "hidden");
     timeContainer.classList.remove("hidden");
     scoreContainer.classList.remove("hidden");
     startTimer();
-    question0();
+    currentQuestion();
 });
 
-if (timer.innerHTML === 0){
-    gameOver.classList.remove("hidden")
+if (timer.textContent === 0) {
+    gameOver.classList.remove("hidden") // can't get this to work
 }
 
-for (let i=0; i < choice.length; i++){
-    choice[i].addEventListener("click", function(){
+for (let i = 0; i < choice.length; i++) {
+    choice[i].addEventListener("click", function correct() {
         if (choice[i].textContent === realAnswer.textContent) {
-            timer.textContent ++;
+            timer.textContent++;
+            score.textContent++;
 
         } else {
             timer.textContent--;
         }
     })
 }
+
+//going nuts over this
+function currentQuestion() {
+    if (i = score.textContent) {
+        questionBox.textContent = questions[i].title;
+        answer0.textContent = questions[i].choices[0];
+        answer1.textContent = questions[i].choices[1];
+        answer2.textContent = questions[i].choices[2];
+        answer3.textContent = questions[i].choices[3];
+        realAnswer.textContent = questions[i].answer;
+
+        if (correct()){
+            i++
+        }
+    }
+};
+
 
 // function nextQuestion(){
 
@@ -111,7 +131,7 @@ var questions = [
     },
     {
         title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
         answer: "console.log"
     }
